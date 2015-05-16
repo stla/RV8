f28d9e2771a4d19d921aa06255d4bb9ff932dee1
stla <laurent_step@yahoo.fr>
2015-05-16 11:44:50 +0200

    libraries

---
+++ inst/js/hbinom/hbinom.js
@@ -0,0 +1,74 @@
+
+//load("../libraries/underscore-min.js");
+load("../libraries/jStat/core.js");
+load("../libraries/jStat/special.js");
+load("../libraries/jStat/distribution.js");
+load("../libraries/jStat/vector.js");
+load("../libraries/jStat/test.js");
+
+
+
+function Power0(Theta, precision, I, J, nsims, U){
+	if(typeof U == 'undefined'){
+		var U = new Array(I);
+		for (var i = 0; i < I; i++) {
+			U[i] = new Array(nsims);
+			for(var sim=0; sim<nsims; sim++){
+				U[i][sim] = Math.random();
+			}
+		}
+	}
+	var a=precision*Theta;
+	var b=precision*(1-Theta);
+	var jBeta=jStat.beta;
+	var lower = 0;
+	var upper = 0;
+	var twosided = 0;
+	for(var sim=0; sim<nsims; sim++){
+		var theta = Array(I);
+		for(var i=0; i<I; i++){
+			theta[i] = jBeta.sample(a,b);
+		}
+		var y = Array(I); 
+		for(var i=0; i<I; i++){
+			y[i] = 0;
+			for(var j=0; j<J; j++){
+				y[i] += (U[i][sim] < theta[i]);
+			}
+			y[i] /= J;
+		}
+		var bounds = jStat.tci(jStat(y).mean(), alpha=.05, y); 
+		lower += (bounds[0] < Theta);
+		upper += (bounds[1] > Theta);
+		twosided += (bounds[0] < Theta) && (bounds[1] > Theta);
+	}
+	return [lower/nsims, upper/nsims, twosided/nsims];
+}
+
+function Power(precision, I, J, nsims, npoints){
+	var lower=[];
+	var upper=[];
+	var twosided=[];
+	var U = new Array(I);
+	for (var i = 0; i < I; i++) {
+		U[i] = new Array(nsims);
+		for(var sim=0; sim<nsims; sim++){
+			U[i][sim] = Math.random();
+		}
+	}
+	for(var i=1; i<npoints; i++){
+		var Theta=i/npoints;
+		var power=Power0(Theta, precision, I, J, nsims, U);
+		lower.push([Theta, power[0]]);
+		upper.push([Theta, power[1]]);
+		twosided.push([Theta, power[2]]);		
+	}
+	return [lower,upper,twosided];
+}
+
+
+print(JSON.stringify(Power0(0.5, 100, 10, 12, nsims=5000)))
+
+var pow=Power(100, 10, 12, nsims=500, npoints=3);
+print(JSON.stringify(pow))
+print(JSON.stringify(pow[0]))
