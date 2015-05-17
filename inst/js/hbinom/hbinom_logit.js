18120bbd60a4f056c9f7cd33f41c05a7ba013d52
stla <laurent_step@yahoo.fr>
2015-05-17 12:18:24 +0200

    hbinom logit

--- inst/js/hbinom/hbinom_logit.js
+++ inst/js/hbinom/hbinom_logit.js
@@ -6,7 +6,8 @@ load("../libraries/jStat/distribution.js");
 load("../libraries/jStat/vector.js");
 load("../libraries/jStat/test.js");
 
-
+function logit(p) { return Math.log(p)-Math.log(1-p); }
+function expit(x) { return Math.exp(x)/(1+Math.exp(x)); }
 
 function Power0(Theta, precision, I, J, nsims, U, Z){
 	if(typeof U == 'undefined'){
@@ -22,33 +23,29 @@ function Power0(Theta, precision, I, J, nsims, U, Z){
 		}
 	}
 	if(typeof Z == 'undefined'){
-		var U = new Array(I);
+		var jGauss = jStat.normal;
+		var Z = new Array(I);
 		for (var i = 0; i < I; i++) {
-			U[i] = new Array(nsims);
+			Z[i] = new Array(nsims);
 			for(var sim=0; sim<nsims; sim++){
-				U[i][sim] = new Array(J);
-				for(var j=0; j<J; j++){
-					U[i][sim][j] = Math.random();
-				}
+				Z[i][sim] =  jGauss.sample(0,1);
 			}
 		}
 	}
-	var a=precision*Theta;
-	var b=precision*(1-Theta);
-	var jBeta=jStat.beta;
+	var sigma= Math.sqrt(1/precision);
 	var lower = 0;
 	var upper = 0;
 	var twosided = 0;
 	for(var sim=0; sim<nsims; sim++){
 		var theta = Array(I);
 		for(var i=0; i<I; i++){
-			theta[i] = jBeta.sample(a,b);
+			theta[i] = Theta;// expit(logit(Theta) + sigma*Z[i][sim]);
 		}
 		var y = Array(I); 
 		for(var i=0; i<I; i++){
 			y[i] = 0;
 			for(var j=0; j<J; j++){
-				y[i] += (U[i][sim] < theta[i]);
+				y[i] += (U[i][sim][j] < theta[i]);
 			}
 			y[i] /= J;
 		}
@@ -60,7 +57,7 @@ function Power0(Theta, precision, I, J, nsims, U, Z){
 	return [lower/nsims, upper/nsims, twosided/nsims];
 }
 
-function Power(precision, I, J, nsims, npoints, U){
+function Power(precision, I, J, nsims, npoints, U, Z){
 	var lower=[];
 	var upper=[];
 	var twosided=[];
@@ -77,10 +74,22 @@ function Power(precision, I, J, nsims, npoints, U){
 		}
 	} else {
 		U = U.slice(0,I);
-	}	
+	}
+	if(typeof Z == 'undefined'){
+		var jGauss = jStat.normal;
+		var Z = new Array(I);
+		for (var i = 0; i < I; i++) {
+			Z[i] = new Array(nsims);
+			for(var sim=0; sim<nsims; sim++){
+				Z[i][sim] =  jGauss.sample(0,1);
+			}
+		}
+	} else {
+		Z = Z.slice(0,I);
+	}
 	for(var i=1; i<npoints; i++){
 		var Theta=i/npoints;
-		var power=Power0(Theta, precision, I, J, nsims, U);
+		var power=Power0(Theta, precision, I, J, nsims, U, Z);
 		lower.push([Theta, power[0]]);
 		upper.push([Theta, power[1]]);
 		twosided.push([Theta, power[2]]);		
@@ -89,7 +98,7 @@ function Power(precision, I, J, nsims, npoints, U){
 }
 
 
-////print(JSON.stringify(Power0(0.5, 100, 10, 12, nsims=5000)))
+print(JSON.stringify(Power0(0.5, 1, 10, 12, nsims=5000)))
 //var I = 100;
 //var nsims=5000;
 //var U = new Array(I);
@@ -100,8 +109,8 @@ function Power(precision, I, J, nsims, npoints, U){
 	//}
 //}
 		
-//~ var pow=Power(100, 10, 12, nsims=500, npoints=3);
-//~ print(JSON.stringify(pow))
+//var pow=Power(100, 10, 12, nsims=500, npoints=3);
+//print(JSON.stringify(pow))
 //print(JSON.stringify(pow[0]))
 
 
